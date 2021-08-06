8a77e85fe2aa92d385ff4a41be995427461d92d1
ebakos <ebakos@cern.ch>
2021-08-06 17:27:53 +0200

    Added Event generation

---
+++ AnalysisTool/obj/analysis/reconstruction/RecoAnalysis.d
@@ -0,0 +1,225 @@
+obj/analysis/reconstruction/RecoAnalysis.o: src/analysis/reconstruction/RecoAnalysis.cpp \
+ src/analysis/reconstruction/RecoAnalysis.hpp \
+ /home/ebakos/Projects/root/include/TClonesArray.h \
+ /home/ebakos/Projects/root/include/Rtypes.h \
+ /home/ebakos/Projects/root/include/RtypesCore.h \
+ /home/ebakos/Projects/root/include/ROOT/RConfig.hxx \
+ /home/ebakos/Projects/root/include/ROOT/../RVersion.h \
+ /home/ebakos/Projects/root/include/RConfigure.h \
+ /home/ebakos/Projects/root/include/DllImport.h \
+ /home/ebakos/Projects/root/include/snprintf.h \
+ /home/ebakos/Projects/root/include/strlcpy.h \
+ /home/ebakos/Projects/root/include/TGenericClassInfo.h \
+ /home/ebakos/Projects/root/include/TSchemaHelper.h \
+ /home/ebakos/Projects/root/include/TIsAProxy.h \
+ /home/ebakos/Projects/root/include/TVirtualIsAProxy.h \
+ /home/ebakos/Projects/root/include/TObjArray.h \
+ /home/ebakos/Projects/root/include/TSeqCollection.h \
+ /home/ebakos/Projects/root/include/TCollection.h \
+ /home/ebakos/Projects/root/include/TObject.h \
+ /home/ebakos/Projects/root/include/RConfigure.h \
+ /home/ebakos/Projects/root/include/TStorage.h \
+ /home/ebakos/Projects/root/include/TVersionCheck.h \
+ /home/ebakos/Projects/root/include/RVersion.h \
+ /home/ebakos/Projects/root/include/TIterator.h \
+ /home/ebakos/Projects/root/include/TString.h \
+ /home/ebakos/Projects/root/include/TMathBase.h \
+ /home/ebakos/Projects/root/include/ROOT/RStringView.hxx \
+ /home/ebakos/Projects/root/include/ROOT/RWrap_libcpp_string_view.h \
+ /home/ebakos/Projects/root/include/ROOT/libcpp_string_view.h \
+ /home/ebakos/Projects/root/include/ROOT/TypeTraits.hxx \
+ /home/ebakos/Projects/root/include/ROOT/RSpan.hxx \
+ /home/ebakos/Projects/root/include/ROOT/span.hxx \
+ /home/ebakos/Projects/root/include/TVirtualRWMutex.h \
+ /home/ebakos/Projects/root/include/TVirtualMutex.h \
+ /home/ebakos/Projects/MadGraph/MG5_aMC_v3_1_0/Delphes/classes/DelphesClasses.h \
+ /home/ebakos/Projects/root/include/TLorentzVector.h \
+ /home/ebakos/Projects/root/include/TMath.h \
+ /home/ebakos/Projects/root/include/TError.h \
+ /home/ebakos/Projects/root/include/TVector3.h \
+ /home/ebakos/Projects/root/include/TVector2.h \
+ /home/ebakos/Projects/root/include/TMatrix.h \
+ /home/ebakos/Projects/root/include/TMatrixF.h \
+ /home/ebakos/Projects/root/include/TMatrixT.h \
+ /home/ebakos/Projects/root/include/TMatrixTBase.h \
+ /home/ebakos/Projects/root/include/TMatrixFBasefwd.h \
+ /home/ebakos/Projects/root/include/TMatrixDBasefwd.h \
+ /home/ebakos/Projects/root/include/TVectorFfwd.h \
+ /home/ebakos/Projects/root/include/TVectorDfwd.h \
+ /home/ebakos/Projects/root/include/TMatrixTUtils.h \
+ /home/ebakos/Projects/root/include/TMatrixFfwd.h \
+ /home/ebakos/Projects/root/include/TMatrixFUtils.h \
+ /home/ebakos/Projects/root/include/TMatrixFUtilsfwd.h \
+ /home/ebakos/Projects/root/include/TRotation.h \
+ /home/ebakos/Projects/root/include/TObject.h \
+ /home/ebakos/Projects/root/include/TRef.h \
+ /home/ebakos/Projects/root/include/TRefArray.h \
+ /home/ebakos/Projects/root/include/TProcessID.h \
+ /home/ebakos/Projects/root/include/TNamed.h \
+ /home/ebakos/Projects/MadGraph/MG5_aMC_v3_1_0/Delphes/classes/SortableObject.h \
+ /home/ebakos/Projects/root/include/TMath.h \
+ /home/ebakos/Projects/MadGraph/MG5_aMC_v3_1_0/Delphes/external/ExRootAnalysis/ExRootTreeReader.h \
+ /home/ebakos/Projects/root/include/TChain.h \
+ /home/ebakos/Projects/root/include/TTree.h \
+ /home/ebakos/Projects/root/include/Compression.h \
+ /home/ebakos/Projects/root/include/ROOT/TIOFeatures.hxx \
+ /home/ebakos/Projects/root/include/TBasket.h \
+ /home/ebakos/Projects/root/include/TKey.h \
+ /home/ebakos/Projects/root/include/TDatime.h \
+ /home/ebakos/Projects/root/include/TBuffer.h \
+ /home/ebakos/Projects/root/include/TDataType.h \
+ /home/ebakos/Projects/root/include/TDictionary.h \
+ /home/ebakos/Projects/root/include/ESTLType.h \
+ /home/ebakos/Projects/root/include/TClass.h \
+ /home/ebakos/Projects/root/include/TObjString.h \
+ /home/ebakos/Projects/root/include/ThreadLocalStorage.h \
+ /home/ebakos/Projects/root/include/Bytes.h \
+ /home/ebakos/Projects/root/include/Byteswap.h \
+ /home/ebakos/Projects/root/include/TArrayD.h \
+ /home/ebakos/Projects/root/include/TArray.h \
+ /home/ebakos/Projects/root/include/TArrayI.h \
+ /home/ebakos/Projects/root/include/TAttFill.h \
+ /home/ebakos/Projects/root/include/TAttLine.h \
+ /home/ebakos/Projects/root/include/TAttMarker.h \
+ /home/ebakos/Projects/root/include/TBranch.h \
+ /home/ebakos/Projects/root/include/TBranchCacheInfo.h \
+ /home/ebakos/Projects/root/include/TBits.h \
+ /home/ebakos/Projects/root/include/TDirectory.h \
+ /home/ebakos/Projects/root/include/TList.h \
+ /home/ebakos/Projects/root/include/TUUID.h \
+ /home/ebakos/Projects/root/include/TVirtualTreePlayer.h \
+ /home/ebakos/Projects/root/include/TFile.h \
+ /home/ebakos/Projects/root/include/TDirectoryFile.h \
+ /home/ebakos/Projects/root/include/TMap.h \
+ /home/ebakos/Projects/root/include/THashTable.h \
+ /home/ebakos/Projects/root/include/TUrl.h \
+ /home/ebakos/Projects/root/include/ROOT/RConcurrentHashColl.hxx \
+ /home/ebakos/Projects/root/include/Rtypes.h \
+ /home/ebakos/Projects/root/include/ROOT/TRWSpinLock.hxx \
+ /home/ebakos/Projects/root/include/ROOT/TSpinMutex.hxx \
+ /home/ebakos/Projects/root/include/TNamed.h \
+ /home/ebakos/Projects/root/include/TROOT.h src/analysis/AnalysisTool.hpp \
+ /home/ebakos/Projects/root/include/TH1.h \
+ /home/ebakos/Projects/root/include/TAxis.h \
+ /home/ebakos/Projects/root/include/TAttAxis.h \
+ /home/ebakos/Projects/root/include/TArrayC.h \
+ /home/ebakos/Projects/root/include/TArrayS.h \
+ /home/ebakos/Projects/root/include/TArrayF.h \
+ /home/ebakos/Projects/root/include/Foption.h \
+ /home/ebakos/Projects/root/include/Fit/FitExecutionPolicy.h \
+ /home/ebakos/Projects/root/include/TFitResultPtr.h \
+ /home/ebakos/Projects/root/include/TH2.h \
+ /home/ebakos/Projects/root/include/TH1.h
+src/analysis/reconstruction/RecoAnalysis.cpp:
+src/analysis/reconstruction/RecoAnalysis.hpp:
+/home/ebakos/Projects/root/include/TClonesArray.h:
+/home/ebakos/Projects/root/include/Rtypes.h:
+/home/ebakos/Projects/root/include/RtypesCore.h:
+/home/ebakos/Projects/root/include/ROOT/RConfig.hxx:
+/home/ebakos/Projects/root/include/ROOT/../RVersion.h:
+/home/ebakos/Projects/root/include/RConfigure.h:
+/home/ebakos/Projects/root/include/DllImport.h:
+/home/ebakos/Projects/root/include/snprintf.h:
+/home/ebakos/Projects/root/include/strlcpy.h:
+/home/ebakos/Projects/root/include/TGenericClassInfo.h:
+/home/ebakos/Projects/root/include/TSchemaHelper.h:
+/home/ebakos/Projects/root/include/TIsAProxy.h:
+/home/ebakos/Projects/root/include/TVirtualIsAProxy.h:
+/home/ebakos/Projects/root/include/TObjArray.h:
+/home/ebakos/Projects/root/include/TSeqCollection.h:
+/home/ebakos/Projects/root/include/TCollection.h:
+/home/ebakos/Projects/root/include/TObject.h:
+/home/ebakos/Projects/root/include/RConfigure.h:
+/home/ebakos/Projects/root/include/TStorage.h:
+/home/ebakos/Projects/root/include/TVersionCheck.h:
+/home/ebakos/Projects/root/include/RVersion.h:
+/home/ebakos/Projects/root/include/TIterator.h:
+/home/ebakos/Projects/root/include/TString.h:
+/home/ebakos/Projects/root/include/TMathBase.h:
+/home/ebakos/Projects/root/include/ROOT/RStringView.hxx:
+/home/ebakos/Projects/root/include/ROOT/RWrap_libcpp_string_view.h:
+/home/ebakos/Projects/root/include/ROOT/libcpp_string_view.h:
+/home/ebakos/Projects/root/include/ROOT/TypeTraits.hxx:
+/home/ebakos/Projects/root/include/ROOT/RSpan.hxx:
+/home/ebakos/Projects/root/include/ROOT/span.hxx:
+/home/ebakos/Projects/root/include/TVirtualRWMutex.h:
+/home/ebakos/Projects/root/include/TVirtualMutex.h:
+/home/ebakos/Projects/MadGraph/MG5_aMC_v3_1_0/Delphes/classes/DelphesClasses.h:
+/home/ebakos/Projects/root/include/TLorentzVector.h:
+/home/ebakos/Projects/root/include/TMath.h:
+/home/ebakos/Projects/root/include/TError.h:
+/home/ebakos/Projects/root/include/TVector3.h:
+/home/ebakos/Projects/root/include/TVector2.h:
+/home/ebakos/Projects/root/include/TMatrix.h:
+/home/ebakos/Projects/root/include/TMatrixF.h:
+/home/ebakos/Projects/root/include/TMatrixT.h:
+/home/ebakos/Projects/root/include/TMatrixTBase.h:
+/home/ebakos/Projects/root/include/TMatrixFBasefwd.h:
+/home/ebakos/Projects/root/include/TMatrixDBasefwd.h:
+/home/ebakos/Projects/root/include/TVectorFfwd.h:
+/home/ebakos/Projects/root/include/TVectorDfwd.h:
+/home/ebakos/Projects/root/include/TMatrixTUtils.h:
+/home/ebakos/Projects/root/include/TMatrixFfwd.h:
+/home/ebakos/Projects/root/include/TMatrixFUtils.h:
+/home/ebakos/Projects/root/include/TMatrixFUtilsfwd.h:
+/home/ebakos/Projects/root/include/TRotation.h:
+/home/ebakos/Projects/root/include/TObject.h:
+/home/ebakos/Projects/root/include/TRef.h:
+/home/ebakos/Projects/root/include/TRefArray.h:
+/home/ebakos/Projects/root/include/TProcessID.h:
+/home/ebakos/Projects/root/include/TNamed.h:
+/home/ebakos/Projects/MadGraph/MG5_aMC_v3_1_0/Delphes/classes/SortableObject.h:
+/home/ebakos/Projects/root/include/TMath.h:
+/home/ebakos/Projects/MadGraph/MG5_aMC_v3_1_0/Delphes/external/ExRootAnalysis/ExRootTreeReader.h:
+/home/ebakos/Projects/root/include/TChain.h:
+/home/ebakos/Projects/root/include/TTree.h:
+/home/ebakos/Projects/root/include/Compression.h:
+/home/ebakos/Projects/root/include/ROOT/TIOFeatures.hxx:
+/home/ebakos/Projects/root/include/TBasket.h:
+/home/ebakos/Projects/root/include/TKey.h:
+/home/ebakos/Projects/root/include/TDatime.h:
+/home/ebakos/Projects/root/include/TBuffer.h:
+/home/ebakos/Projects/root/include/TDataType.h:
+/home/ebakos/Projects/root/include/TDictionary.h:
+/home/ebakos/Projects/root/include/ESTLType.h:
+/home/ebakos/Projects/root/include/TClass.h:
+/home/ebakos/Projects/root/include/TObjString.h:
+/home/ebakos/Projects/root/include/ThreadLocalStorage.h:
+/home/ebakos/Projects/root/include/Bytes.h:
+/home/ebakos/Projects/root/include/Byteswap.h:
+/home/ebakos/Projects/root/include/TArrayD.h:
+/home/ebakos/Projects/root/include/TArray.h:
+/home/ebakos/Projects/root/include/TArrayI.h:
+/home/ebakos/Projects/root/include/TAttFill.h:
+/home/ebakos/Projects/root/include/TAttLine.h:
+/home/ebakos/Projects/root/include/TAttMarker.h:
+/home/ebakos/Projects/root/include/TBranch.h:
+/home/ebakos/Projects/root/include/TBranchCacheInfo.h:
+/home/ebakos/Projects/root/include/TBits.h:
+/home/ebakos/Projects/root/include/TDirectory.h:
+/home/ebakos/Projects/root/include/TList.h:
+/home/ebakos/Projects/root/include/TUUID.h:
+/home/ebakos/Projects/root/include/TVirtualTreePlayer.h:
+/home/ebakos/Projects/root/include/TFile.h:
+/home/ebakos/Projects/root/include/TDirectoryFile.h:
+/home/ebakos/Projects/root/include/TMap.h:
+/home/ebakos/Projects/root/include/THashTable.h:
+/home/ebakos/Projects/root/include/TUrl.h:
+/home/ebakos/Projects/root/include/ROOT/RConcurrentHashColl.hxx:
+/home/ebakos/Projects/root/include/Rtypes.h:
+/home/ebakos/Projects/root/include/ROOT/TRWSpinLock.hxx:
+/home/ebakos/Projects/root/include/ROOT/TSpinMutex.hxx:
+/home/ebakos/Projects/root/include/TNamed.h:
+/home/ebakos/Projects/root/include/TROOT.h:
+src/analysis/AnalysisTool.hpp:
+/home/ebakos/Projects/root/include/TH1.h:
+/home/ebakos/Projects/root/include/TAxis.h:
+/home/ebakos/Projects/root/include/TAttAxis.h:
+/home/ebakos/Projects/root/include/TArrayC.h:
+/home/ebakos/Projects/root/include/TArrayS.h:
+/home/ebakos/Projects/root/include/TArrayF.h:
+/home/ebakos/Projects/root/include/Foption.h:
+/home/ebakos/Projects/root/include/Fit/FitExecutionPolicy.h:
+/home/ebakos/Projects/root/include/TFitResultPtr.h:
+/home/ebakos/Projects/root/include/TH2.h:
+/home/ebakos/Projects/root/include/TH1.h:
